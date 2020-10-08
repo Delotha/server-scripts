01c10acd2c3d86bb828ecba5b8f76929247d806b
Elyrith <elyrith@gmail.com>
2020-10-08 01:37:38 -0400

    Add a check of the config file to make sure we're actually running on an archive server. Also made the log file go to ~/ rather than /tmp.

--- archive-server.sh
+++ archive-server.sh
@@ -9,20 +9,26 @@ time=$(date +%H:%M:%S)
 configfile="/etc/server-scripts/$HOSTNAME.config"
 
 # Update log file that we've started
-echo "$date-$time: Started." >> /tmp/loudfoot-archive-server.log
+echo "$date-$time: Started." >> ~/loudfoot-archive-server.log
 
 # Get the config file
 if [ -e "$configfile" ]; then
 	source "$configfile"
 	sitedest="$dest/sites"
 else
-	echo "$date-$time: Error: Config file not found ($configfile)." >> /tmp/loudfoot-archive-server.log
+	echo "$date-$time: Error: Config file not found ($configfile)." >> ~/loudfoot-archive-server.log
 	exit 0
 fi
 
+if [ -z $userdest ]; then
+	cleanup=$(rm ~/loudfoot-archive-server.log)
+	exit 0 # Exiting. This is not a server that gets backed up.
+fi
+
+
 whoami=$(whoami)
 if [ $whoami != $userdest ]; then
-	echo "$date-$time: Error: Not running as the correct user ($whoami instead of $whois)" >> /tmp/loudfoot-archive-server.log
+	echo "$date-$time: Error: Not running as the correct user ($whoami instead of $whois)" >> ~/loudfoot-archive-server.log
 fi
 
 for i in "${servers[@]}"
@@ -45,6 +51,6 @@ esac
 done
 
 # Update log file of our success
-echo "$date-$time: Complete." >> /tmp/loudfoot-archive-server.log
+echo "$date-$time: Complete." >> ~/loudfoot-archive-server.log
 
 exit 0
