353e9889381ee338d5c20aca7e8741f69397d3d7
Ryan <ryan@loudfoot.ca>
2020-12-19 17:59:49 -0500

    Updated monthly task ticket script so it only has to run on one server.

--- create-monthly-task-ticket.py
+++ create-monthly-task-ticket.py
@@ -5,52 +5,54 @@ import os
 import datetime
 import requests
 import socket
+import configparser
 
 # Get hostname
 hostname = socket.gethostname()
 
-# Get variables from the config file (which Loudfoot usually keeps in /etc/server-scripts/)
-from dotenv import load_dotenv, find_dotenv
-from pathlib import Path
-env_path = Path('/etc/server-scripts') / hostname + '.env'
-load_dotenv(dotenv_path=env_path)
+# Create parser object and read config file
+# TODO: Check if this file exists. Exit immediately if it doesn't.
+config = configparser.RawConfigParser()
+config.read(hostname+'.cfg')
 
-TOKEN = os.getenv('MANTIS_TOKEN')
-URL = os.getenv('MANTIS_URL')
-CATEGORY = os.getenv('MANTIS_CATEGORY')
-PROJECT = os.getenv('MANTIS_PROJECT')
-DESCRIPTION = os.getenv('MANTIS_MONTHLY_DESCRIPTION')
+# Loop through all sections (which are company names)
+for company in config.sections():
 
-# Make sure we found all the required variables
-errors = 0
-if not TOKEN:
-	print('No MANTIS_TOKEN found in dotenv file.')
-	errors += 1
-if not URL:
-	print('No MANTIS_URL found in dotenv file.')
-	errors += 1
-if not CATEGORY:
-	print('No MANTIS_CATEGORY found in dotenv file.')
-	errors += 1
-if not PROJECT:
-	print('No MANTIS_PROJECT found in dotenv file.')
-	errors += 1
-if not DESCRIPTION:
-	print('No MANTIS_MONTHLY_DESCRIPTION found in dotenv file.')
-	errors += 1
-if errors > 0:
-	exit()
+	#TODO Make sure we found all the required variables
+#	errors = 0
+#	if not "mantis_token" in company.keys():
+#		print('No MANTIS_TOKEN found in dotenv file.')
+#		errors += 1
+#	if not "mantis_url" in company.keys():
+#		print('No MANTIS_URL found in dotenv file.')
+#		errors += 1
+#	if not "mantis_category" in company.keys():
+#		print('No MANTIS_CATEGORY found in dotenv file.')
+#		errors += 1
+#	if not "mantis_project" in company.keys():
+#		print('No MANTIS_PROJECT found in dotenv file.')
+#		errors += 1
+#	if not "mantis_monthly_description" in company.keys():
+#		print('No MANTIS_MONTHLY_DESCRIPTION found in dotenv file.')
+#		errors += 1
+#	if errors > 0:
+#		exit()
 
-# Build the ticket name with the YYYY and Month
-now = datetime.datetime.now()
-name = 'Monthly Tasks - ' + now.strftime("%Y %B")
+	# Build the ticket name with the YYYY and Month
+	now = datetime.datetime.now()
+	name = 'Monthly Tasks - ' + now.strftime("%Y %B")
 
-# CURL call stuff:
-api_url = URL + '/api/rest/issues/'
-data = { 'summary': name, 'description': DESCRIPTION, 'category': {'name': CATEGORY}, 'project': {'name': PROJECT} }
-headers = { 'Authorization': TOKEN }
+	# CURL call stuff:
+	api_url = config.get(company, "mantis_url") + '/api/rest/issues/'
+	data = { 'summary': name, 'description': config.get(company, "mantis_monthly_description"), 'category': {'name': config.get(company, "mantis_category")}, 'project': {'name': config.get(company, "mantis_project")} }
+	headers = { 'Authorization': config.get(company, "mantis_token") }
 
-# Make the actual call
-#response = requests.post(api_url, json=data, headers=headers)
+	# Make the actual call
+	response = requests.post(api_url, json=data, headers=headers)
+
+#	Debug stuff 
+#	print(api_url) # DEBUG
+#	print(data) # DEBUG
+#	print(headers) # DEBUG
 
 exit()
