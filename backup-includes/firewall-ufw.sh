792ba00af0f4f40b706c78a02c652cdcd3514574
Ryan <ryan@loudfoot.ca>
2021-07-08 23:22:38 -0400

    Oops. Don't stop the backup just because ufw is active.

--- backup-includes/firewall-ufw.sh
+++ backup-includes/firewall-ufw.sh
@@ -3,14 +3,12 @@
 # Check if ufw is active
 active=$(ufw status | head -1 | grep "active")
 if [[ $active != "" ]]; then
-	exit 0
+	##### ufw Back #####
+	echo "Backup ufw - Start"
+
+	# Take the backups
+	out=$(ufw status >> $dest/ufw-status.txt)
+
+	echo 'Backup ufw - Done'
+	##### ufw Done #####
 fi
-
-##### ufw Back #####
-echo "Backup ufw - Start"
-
-# Take the backups
-out=$(ufw status >> $dest/ufw-status.txt)
-
-echo 'Backup ufw - Done'
-##### ufw Done #####
